b61ceba0df604c48f3b6947ad2ec15a3bd13c3d9
evaderrama <evalderrama@domingoalonsogroup.com>
2022-06-06 23:24:39 +0100

    Composite design patter

---
+++ ESTRUCTURAL/COMPOSITE/CodeExample/Composite/Component.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    internal abstract class Component
+    {
+        //La clase base Component declara opreaciones comunes para objetos simples y complejos de una composicion
+        public Component () { }
+
+        //El componente base puede implementar algún comportamiento predeterminado o dejarlo a clases concretas (declarando el método que contiene el comportamiento como "abstracto")
+        public abstract string Operation();
+
+        // En algunos casos, sería beneficioso definir la gestión de los hijos de esta manera no haras opreaciones directametne en la clase de componente.
+        // Se necesita exponer cualquier clase de componente concreto al código del cliente, incluso durante el ensamblaje del árbol de objetos. La desventaja es que estos métodos estarán vacíos para los componentes a nivel de hoja.
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Podemos proporcionar un método que permita que el código del cliente descubra si un componente puede tener hijos.
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+
+    internal class Hoja: Component
+    {
+        public override string Operation()
+        {
+            return "Hoja";
+        }
+
+        public override bool IsComposite()
+        {
+            return false;
+        }
+    }
+
+    // La clase Composite representa los componentes complejos que pueden tener hijos. Por lo general, los objetos compuestos delegan el trabajo real a sus hijos y luego "resumir" el resultado.
+    internal class Composite : Component
+    {
+        protected List<Component> _Hijo = new List<Component>();
+
+       
+        public override void Add(Component component)
+        {
+            this._Hijo.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            this._Hijo.Remove(component);
+        }
+
+        // El Composite ejecuta su lógica primaria de una manera particular. Eso atraviesa recursivamente a través de todos sus hijos, recopilando y sumando sus resultados.
+        // Dado que los hijos del compuesto llama a sus hijos y así sucesivamente, sucesivamente toodo el árbol de objetos se recorre como resultado 
+        public override string Operation()
+        {
+           int cont = 0;
+           string result = "Rama(";
+
+           foreach (Component component in _Hijo)
+           {
+               result += component.Operation();
+               if (cont != _Hijo.Count - 1)
+               {
+                   result += "+";
+               }
+           }
+           return result + ")";
+
+        }
+
+        public class Client
+        {
+            //El cliente trabaja con todos los implicados gracias a la clase base interfaz (internal abstract class Component)
+            public void ClientCode(Component hoja)
+            {
+                Console.WriteLine($"Resultado: {hoja.Operation()}\n");
+            }
+        }
+    }
+}
---
+++ ESTRUCTURAL/COMPOSITE/CodeExample/Composite/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+using Composite;
+
+namespace Composite
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Composite.Client client = new Composite.Client();
+
+            //De esta manera el clodigo del cliente puede soportar componentes simples de tipo hoja
+            var hoja = new Hoja();
+            Console.WriteLine("Cliente: Tengo un componente sencillo:");
+            client.ClientCode(hoja);
+
+            //Tambien puede soportar  los objetos compuestos complejos
+            Composite arbol = new Composite();
+
+            Composite rama1 = new Composite();
+            rama1.Add(new Hoja());
+            rama1.Add(new Hoja());
+
+            Composite rama2 = new Composite();
+            rama2.Add(new Hoja());
+
+            arbol.Add(rama1);
+            arbol.Add(rama2);
+            Console.WriteLine("Cliente: Ahora tenemos un arbol compuesto");
+            client.ClientCode(arbol);
+
+            Console.WriteLine("Cliente: No necesito verificar las clases de componentes incluso cuando administro el árbol ");
+
+        }
+    }
+}
