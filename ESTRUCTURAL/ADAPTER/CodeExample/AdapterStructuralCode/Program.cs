a92319a3f306987dca8426e1ceda92c3f98ee9ad
evaderrama <evalderrama@domingoalonsogroup.com>
2022-06-13 23:31:41 +0100

    :sparkles: Structural Adapter Code example added

---
+++ ESTRUCTURAL/ADAPTER/CodeExample/AdapterStructuralCode/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AdapterStructuralCode
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Adapter Desing Pattern, ejemplo de estructura de código 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Codigo original heredado con dependencias
+            Customer customer = new Customer();
+            customer.FirstName();
+
+            //Esperando por el usuario
+            Console.ReadKey();
+
+            //Creando el adaptador y añadiendole un método
+            Customer customerAdapter = new Adapter();
+            customerAdapter.FirstName();
+
+        }
+    }
+
+    /// <summary>
+    /// La clase cliente
+    /// </summary>
+    internal class Customer
+    {
+        public virtual void FirstName()
+        {
+            Console.WriteLine("Llamando al metodo FirstName de la clase Customer");
+        }
+    }
+
+    internal class Adapter : Customer
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void FirstName()
+        {
+            //Posibilidad de hacer otro trabajo y despues llamar al metodo especifico
+            adaptee.SpecificFirstName();
+        }
+    }
+
+    internal class Adaptee
+    {
+        public void SpecificFirstName()
+        {
+            Console.WriteLine("Llamando al metodo especifico ");
+        }
+    }
+}
